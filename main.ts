55a7841e6642360a7ab33550112f9a6f8b694436
Denken lernen - Probleme lösen <83607557+dlpl-mb@users.noreply.github.com>
2023-03-02 22:00:20 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 input.onButtonPressed(Button.A, function () {
-	
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . # . #
+        . # # # .
+        # . # . .
+        . # . # .
+        # . . . #
+        `)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . # # # .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . # # # .
+        . # . # .
+        # . . . #
+        `)
 })
 basic.showLeds(`
     . . # . .
